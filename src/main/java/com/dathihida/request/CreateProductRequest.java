8cf577f49c2e758f93b34d73da82ff4e0970d033
Phùng Văn Tiến Đạt <tiendat060709@gmail.com>
2025-01-21 21:42:15 +0700

    fix: quantity and images product item is 0 and null

--- src/main/java/com/dathihida/request/CreateProductRequest.java
+++ src/main/java/com/dathihida/request/CreateProductRequest.java
@@ -2,16 +2,18 @@ package com.dathihida.request;
 
 import lombok.Data;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Data
 public class CreateProductRequest {
     private String title;
     private String description;
+    private int quantity;
     private int mrpPrice;
     private int sellingPrice;
     private String color;
-    private List<String> image;
+    private List<String> images;
     private String category;
     private String category2;
     private String category3;
